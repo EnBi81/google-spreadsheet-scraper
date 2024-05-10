73e55d580b9f4c6909a7a868cdd614cc7da2bcc7
Enbi81 <wotng2002@gmail.com>
2024-05-10 17:31:00 +0200

    commit stuff in there

--- index.mjs
+++ index.mjs
@@ -1,18 +1,22 @@
-const { google } = require('googleapis');
-const express = require('express');
-const open = require('open');
-const fs = require('fs');
-const path = require('path');
+// Import the necessary modules
+import { google } from 'googleapis';
+import express from 'express';
+import open from 'open';
+import fs from 'fs';
+import {config} from 'dotenv'
+
+config()
 
 const app = express();
 const PORT = 3000;
 
 // Load client secrets from a local file.
-const credentials = JSON.parse(fs.readFileSync('credentials.json'));
-const { client_secret, client_id, redirect_uris } = credentials.web;
+const clientId = process.env.GOOGLE_CLIENT_ID;
+const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+const redirect_uri = process.env.GOOGLE_CLIENT_REDIRECT_URI;
 
 const oAuth2Client = new google.auth.OAuth2(
-    client_id, client_secret, redirect_uris[0]
+    clientId, clientSecret, redirect_uri
 );
 
 // Scopes for read-only access to Google Sheets
@@ -21,26 +25,35 @@ const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 // Open an authorization URL in the user's browser
 app.get('/auth', (req, res) => {
     const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
         scope: SCOPES,
+        client_id: clientId,
+        redirect_uri: redirect_uri,
+        response_type: 'token',
+        state: 'state-123'
     });
+
     open(authUrl);
     res.send('Authentication started...');
 });
 
 // Handle the OAuth2 callback
-app.get('/oauth2callback', async (req, res) => {
+app.get('/google-auth-done', async (req, res) => {
     const { code } = req.query;
+
     const { tokens } = await oAuth2Client.getToken(code);
     oAuth2Client.setCredentials(tokens);
 
     readSheet(); // Read data from the spreadsheet
-    res.send('Authentication successful!');
+    res.send('Authentication successful! your code is ' + code);
 });
+
+app.get('/', async (req, res) => {
+    res.send('<a href="/auth"><button>Log in</button></a>')
+})
 
 async function readSheet() {
     const sheets = google.sheets({ version: 'v4', auth: oAuth2Client });
-    const spreadsheetId = 'YOUR_SPREADSHEET_ID';
+    const spreadsheetId = process.env.SPREADSHEET_ID;
     const range = 'Sheet1!A1:E10';
 
     try {

340fce30f2c4e074c494d305a392afca8a97cbef
Enbi81 <wotng2002@gmail.com>
2024-05-10 17:41:45 +0200

    commit stuff in there

--- index.mjs
+++ index.mjs
@@ -8,7 +8,7 @@ import {config} from 'dotenv'
 config()
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.WEB_APP_PORT;
 
 // Load client secrets from a local file.
 const clientId = process.env.GOOGLE_CLIENT_ID;
